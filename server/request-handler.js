25e1d9f549e153edbb32d6ca4b554e227be1cb26
MKS-students <mks.students@hackreactor.com>
2015-08-17 16:07:44 -0500

    Basic server running

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,8 +11,9 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+// var exports = {};
 
-var requestHandler = function(request, response) {
+exports.requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
